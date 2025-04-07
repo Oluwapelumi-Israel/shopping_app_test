d30126cdc1e87959a462940c29217fe8961b2ff5
Israel Oluwapelumi <israeloluwapelumi@gmail.com>
2025-04-07 16:05:06 +0100

    Updated Shopping app

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
 import 'package:device_preview_plus/device_preview_plus.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'views/home.dart';
 
 
 void main() => runApp(
-  DevicePreview(
-    enabled: !kReleaseMode,
-    builder: (context) => MyApp(), // Wrap your app
-  ),
+  MyApp(),
 );
 
 class MyApp extends StatelessWidget {
@@ -18,7 +14,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
       locale: DevicePreview.locale(context),
       title: 'Wallet',
